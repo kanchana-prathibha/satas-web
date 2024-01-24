abba3d871a367d2dd9cdd8a1ec8b6db955129a7b
kanchana <kanchanaprathibha2@gmail.com>
2024-01-24 15:16:00 +0530

    all-ok-rady to host

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js'
 import cookieParser from "cookie-parser";
+import  path  from "path";
+
 dotenv.config();
 const app =express();
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -13,7 +15,9 @@ mongoose.connect(process.env.MONGO).then(()=>{
     
 }).catch((err)=>{
     console.log(err)
-})
+});
+
+const __dirname = path.resolve();
     
 app.use (express.json());
 
@@ -23,12 +27,19 @@ app.listen(3000,()=>{
     console.log('server is running');
 }
 );
+
 app.use('/api/user', userRouter); 
 
 app.use('/api/auth', authRouter);
 
 app.use('/api/listing', listingRouter);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req,res)=>{
+res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+
+});
 app.use((err,req,res,next)=>{
     const statusCode= err.statusCode ||500;
     const message=err.message||"internal server error"; 
